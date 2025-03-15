08b7c50ae992fb3f6db0900068bed330f6e3d35b
binteabid <abidsanaonly@gmail.com>
2025-03-16 00:40:03 +0500

    786

---
+++ app.js
@@ -0,0 +1,8 @@
+
+
+document.write("<h3>Weather App</h3>")
+
+let weatherInCelsius= +prompt("Enter the current weather of your city in °C");
+let cityName=prompt("Enter your city name");
+console.log("The current weather of "+cityName+" is " +weatherInCelsius+ " °C.");
+document.write("<br> The current weather of "+cityName+" is " +weatherInCelsius+ " °C.")
