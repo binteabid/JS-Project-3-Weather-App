8c01432236f1035ebdbd233f7673555675e8f09d
binteabid <abidsanaonly@gmail.com>
2025-03-18 03:03:18 +0500

    format

--- app.js
+++ app.js
@@ -44,14 +44,3 @@ if (weatherInCelsius > 45) {
 
 
 
-// console.log("The current weather of "+cityName+" is " +weatherInCelsius+ " °C.");
-// document.write("<br> The current weather of "+cityName+" is " +weatherInCelsius+ " °C.")
-
-
-/*<div class="card" style="width: 18rem;">
-<img src="..." class="card-img-top" alt="...">
-<div class="card-body">
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-        content.</p>
-</div>
-</div>*/
